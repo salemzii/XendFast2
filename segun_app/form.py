4dca1286c3e690ee89981c1044f567ea46e7a304
salemzii <salemododa2@gmail.com>
2020-12-18 06:24:09 -0800

    hosting with pythonanywhere, lol lets try this again....

---
+++ segun_app/form.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50)
+    middle_name = forms.CharField(max_length=50, required=False)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'middle_name', 'last_name', 'username',
+                  'email','password1', 'password2']
